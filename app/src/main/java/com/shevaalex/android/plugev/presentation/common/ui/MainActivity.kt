e1940d13976afa1d9280cdebe8fd2ccd03b25994
AlexMason <shevaalex@yahoo.co.uk>
2021-05-05 21:00:28 +0100

    add FusedLocationProviderClient and additional permission

--- app/src/main/java/com/shevaalex/android/plugev/presentation/common/ui/MainActivity.kt
+++ app/src/main/java/com/shevaalex/android/plugev/presentation/common/ui/MainActivity.kt
@@ -7,22 +7,24 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import com.google.accompanist.insets.ProvideWindowInsets
 import com.shevaalex.android.plugev.presentation.common.compose.PlugEvTheme
 import com.shevaalex.android.plugev.presentation.mapscreen.MapScreen
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.shevaalex.android.plugev.presentation.common.compose.WhiteTranslucent70
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
+        val fusedLocationProvider = LocationServices.getFusedLocationProviderClient(this)
         setContent {
             ProvideWindowInsets {
                 PlugEvTheme {
-                    MainScreen()
+                    MainScreen(locationProviderClient = fusedLocationProvider)
                 }
             }
         }
@@ -30,19 +32,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
+fun MainScreen(modifier: Modifier = Modifier, locationProviderClient: FusedLocationProviderClient) {
     val systemUiController = rememberSystemUiController()
     val useDarkIcons = MaterialTheme.colors.isLight
     SideEffect {
         systemUiController.setSystemBarsColor(WhiteTranslucent70, darkIcons = useDarkIcons)
     }
-    MapScreen(modifier)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PlugEvTheme {
-        MainScreen()
-    }
+    MapScreen(modifier, locationProviderClient)
 }
