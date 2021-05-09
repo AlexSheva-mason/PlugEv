d4fa68953c04dd953ad4134c49531cbb3e3af77f
AlexMason <shevaalex@yahoo.co.uk>
2021-05-09 17:07:12 +0100

    hilt di initial setup

---
+++ app/src/main/java/com/shevaalex/android/plugev/PlugEvApplication.kt
@@ -0,0 +1,7 @@
+package com.shevaalex.android.plugev
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PlugEvApplication : Application()
---
+++ app/src/main/java/com/shevaalex/android/plugev/di/DataModule.kt
@@ -0,0 +1,31 @@
+package com.shevaalex.android.plugev.di
+
+import com.shevaalex.android.plugev.data.network.service.ChargingStationRetrofitService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val BASE_URL = "https://api.openchargemap.io/v3/poi/"
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class DataModule {
+
+    @Provides
+    fun provideRetrofitBuilder(): Retrofit.Builder {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+    }
+
+    @Provides
+    fun provideChargingStationRetrofitService(builder: Retrofit.Builder): ChargingStationRetrofitService {
+        return builder
+            .build()
+            .create(ChargingStationRetrofitService::class.java)
+    }
+
+}
