fc432c73ed6c16c0a464143ffa9c4707f7fd1126
AlexMason <shevaalex@yahoo.co.uk>
2021-06-08 22:31:55 +0100

    adds fetchRadiusMiles field

--- app/src/main/java/com/shevaalex/android/plugev/presentation/mapscreen/MapScreenViewState.kt
+++ app/src/main/java/com/shevaalex/android/plugev/presentation/mapscreen/MapScreenViewState.kt
@@ -7,6 +7,7 @@ import com.shevaalex.android.plugev.presentation.common.ui.UiState
 data class MapScreenViewState(
     val cameraPosition: LatLng = LatLng(MAP_DEFAULT_LATITUDE, MAP_DEFAULT_LONGITUDE),
     val cameraZoom: Float = MAP_DEFAULT_ZOOM,
+    val fetchRadiusMiles: Float? = null,
     val chargingStations: List<ChargingStation> = listOf(),
     val isLoading: Boolean = true,
     val uiMessage: UiState.UiInfo? = null,
@@ -17,6 +18,7 @@ data class MapScreenViewState(
     override fun toString(): String {
         return "MapScreenViewState(\n cameraPosition=$cameraPosition," +
                 " \n cameraZoom=$cameraZoom," +
+                " \n fetchRadiusMiles=$fetchRadiusMiles," +
                 " \n chargingStations=${chargingStations.size}," +
                 " \n isLoading=$isLoading," +
                 " \n uiMessage=$uiMessage," +
