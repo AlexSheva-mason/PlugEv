9a701345d3863617680ff590d58e242c2a3edaa5
AlexMason <shevaalex@yahoo.co.uk>
2021-06-09 21:38:03 +0100

    change specs of the small shape to percent

--- app/src/main/java/com/shevaalex/android/plugev/presentation/common/compose/Shape.kt
+++ app/src/main/java/com/shevaalex/android/plugev/presentation/common/compose/Shape.kt
@@ -5,7 +5,7 @@ import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(10.dp),
+    small = RoundedCornerShape(percent = 50),
     medium = RoundedCornerShape(14.dp),
     large = RoundedCornerShape(18.dp)
 )
