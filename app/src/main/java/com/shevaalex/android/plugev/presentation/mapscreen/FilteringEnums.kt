5c85e3c050443b859b7cb621fa1f490dda5810f1
AlexMason <shevaalex@yahoo.co.uk>
2021-06-09 22:13:34 +0100

    adds enums for filtering options

---
+++ app/src/main/java/com/shevaalex/android/plugev/presentation/mapscreen/FilteringEnums.kt
@@ -0,0 +1,23 @@
+package com.shevaalex.android.plugev.presentation.mapscreen
+
+enum class FilterOption(
+    val filterType: FilterType,
+    val text: String,
+    var chipState: ChipState
+) {
+    Level1(FilterType.PowerLevel, "Level 1", ChipState.Enabled),
+    Level2(FilterType.PowerLevel, "Level 2", ChipState.Enabled),
+    Level3(FilterType.PowerLevel, "Level 3", ChipState.Enabled),
+    Public(FilterType.Accessibility, "Public", ChipState.Enabled),
+    Private(FilterType.Accessibility, "Private", ChipState.Enabled)
+}
+
+enum class FilterType {
+    PowerLevel,
+    Accessibility
+}
+
+enum class ChipState {
+    Enabled,
+    Disabled
+}
