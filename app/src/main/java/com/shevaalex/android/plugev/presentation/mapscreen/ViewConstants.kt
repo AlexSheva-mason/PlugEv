0f51f8c4f41b5ee49c2411581b4eefce62237893
AlexMason <shevaalex@yahoo.co.uk>
2021-06-12 17:31:52 +0100

    add view size constants

---
+++ app/src/main/java/com/shevaalex/android/plugev/presentation/mapscreen/ViewConstants.kt
@@ -0,0 +1,12 @@
+package com.shevaalex.android.plugev.presentation.mapscreen
+
+import androidx.compose.material.ProgressIndicatorDefaults
+
+const val SNACK_WIDTH_MIN = 100
+const val SNACK_WIDTH_MAX = 344
+
+const val FILTER_CHIP_HEIGHT = 32
+const val FILTER_CHIP_PADDING = 4
+const val FILTER_ROW_PADDING_VERTICAL = 4
+
+val progressBarHeight = ProgressIndicatorDefaults.StrokeWidth.value.toInt()
