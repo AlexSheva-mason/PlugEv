7351807bad39afeda5c5a6ec304f12f29da3f6dc
AlexMason <shevaalex@yahoo.co.uk>
2021-06-13 15:40:33 +0100

    refactor option ids to list of strings

--- app/src/main/java/com/shevaalex/android/plugev/presentation/mapscreen/FilteringRowViewState.kt
+++ app/src/main/java/com/shevaalex/android/plugev/presentation/mapscreen/FilteringRowViewState.kt
@@ -16,23 +16,23 @@ sealed class FilterOption(
     var chipState: ChipState
 ) {
     data class Level1(
-        val id: String = "1"
+        val optionIds: List<String> = listOf("1"),
     ) : FilterOption(FilterType.PowerLevel, "Level 2", ChipState.Enabled)
 
     data class Level2(
-        val id: String = "2"
+        val optionIds: List<String> = listOf("2"),
     ) : FilterOption(FilterType.PowerLevel, "Level 2", ChipState.Enabled)
 
     data class Level3(
-        val id: String = "3"
+        val optionIds: List<String> = listOf("3"),
     ) : FilterOption(FilterType.PowerLevel, "Level 3", ChipState.Enabled)
 
     data class Public(
-        val ids: List<String> = listOf("1", "4", "5", "7")
+        val optionIds: List<String> = listOf("1", "4", "5", "7"),
     ) : FilterOption(FilterType.Accessibility, "Public", ChipState.Enabled)
 
     data class Private(
-        val ids: List<String> = listOf("2", "3", "6")
+        val optionIds: List<String> = listOf("2", "3", "6"),
     ) : FilterOption(FilterType.Accessibility, "Private", ChipState.Enabled)
 }
 
