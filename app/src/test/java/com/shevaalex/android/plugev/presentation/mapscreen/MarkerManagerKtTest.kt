460daca6822e4f7ea5f971fcb149136fbfd26756
AlexMason <shevaalex@yahoo.co.uk>
2021-06-14 22:07:56 +0100

    refactor (extracts stubbing to Before function)

--- app/src/test/java/com/shevaalex/android/plugev/presentation/mapscreen/MarkerManagerKtTest.kt
+++ app/src/test/java/com/shevaalex/android/plugev/presentation/mapscreen/MarkerManagerKtTest.kt
@@ -32,19 +32,19 @@ class MarkerManagerKtTest {
         MockKAnnotations.init(this)
         every { clusterManager.addItem(any()) } returns true
         every { clusterManager.removeItem(any()) } returns true
-    }
-
-    @Test
-    fun `addItemToCollection does not add item if it's out of LatLngBounds`() {
-        //algorithm's collection does not contain 1 item, but it's out of LatLngBounds
         every {
             clusterManager.algorithm.items
         } returns listOf(
             getChargingStationDomainModel(52.20771169949917, 0.13875083384775075),
             getChargingStationDomainModel(52.200584275800814, 0.13395156135782119),
             getChargingStationDomainModel(52.20759197890927, 0.1229459606575915),
-            getChargingStationDomainModel(52.20332286606809, 0.13211238948885104),
+            getChargingStationDomainModel(52.20332286606809, 0.13211238948885104)
         )
+    }
+
+    @Test
+    fun `addItemToCollection does not add item if it's out of LatLngBounds`() {
+        //algorithm's collection does not contain 1 item, but it's out of LatLngBounds
         val chargingStationList = listOf(
             getChargingStationDomainModel(52.20771169949917, 0.13875083384775075),
             getChargingStationDomainModel(52.200584275800814, 0.13395156135782119),
@@ -69,15 +69,6 @@ class MarkerManagerKtTest {
     @Test
     fun `addItemToCollection adds item if it's within LatLngBounds and not in algorithm's collection`() {
         //algorithm's collection does not contain 1 item
-        every {
-            clusterManager.algorithm.items
-        } returns listOf(
-            getChargingStationDomainModel(52.20771169949917, 0.13875083384775075),
-            getChargingStationDomainModel(52.200584275800814, 0.13395156135782119),
-            getChargingStationDomainModel(52.20759197890927, 0.1229459606575915),
-            getChargingStationDomainModel(52.20332286606809, 0.13211238948885104),
-        )
-
         //last item should be added to algorithm's collection
         val itemToAdd = getChargingStationDomainModel(52.20312154620868, 0.13168523135162127)
         val chargingStationList = listOf(
@@ -107,14 +98,6 @@ class MarkerManagerKtTest {
     @Test
     fun `addItemToCollection does not add item if it's already in the algorithm's collection`() {
         //algorithm's collection already contains all items
-        every {
-            clusterManager.algorithm.items
-        } returns listOf(
-            getChargingStationDomainModel(52.20771169949917, 0.13875083384775075),
-            getChargingStationDomainModel(52.200584275800814, 0.13395156135782119),
-            getChargingStationDomainModel(52.20759197890927, 0.1229459606575915),
-            getChargingStationDomainModel(52.20332286606809, 0.13211238948885104),
-        )
         val chargingStationList = listOf(
             getChargingStationDomainModel(52.20771169949917, 0.13875083384775075),
             getChargingStationDomainModel(52.200584275800814, 0.13395156135782119),
@@ -137,14 +120,6 @@ class MarkerManagerKtTest {
 
     @Test
     fun `removeItemFromCollection does not remove item if it's within LatLngBounds`() {
-        every {
-            clusterManager.algorithm.items
-        } returns listOf(
-            getChargingStationDomainModel(52.20771169949917, 0.13875083384775075),
-            getChargingStationDomainModel(52.200584275800814, 0.13395156135782119),
-            getChargingStationDomainModel(52.20759197890927, 0.1229459606575915),
-            getChargingStationDomainModel(52.20332286606809, 0.13211238948885104),
-        )
 
         runBlocking {
             removeItemFromCollection(
@@ -190,6 +165,11 @@ class MarkerManagerKtTest {
                 getChargingStationDomainModel(53.20332286606809, 0.13211238948885104)
             )
         }
+    }
+
+    @Test
+    fun `should delete markers that are not in the viewState's list`() {
+        assert(false)
     }
 
 }
