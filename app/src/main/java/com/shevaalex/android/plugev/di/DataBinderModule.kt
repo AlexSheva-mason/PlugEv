c60610759ce83485f8ee75be1bb753359dd7f416
AlexMason <shevaalex@yahoo.co.uk>
2021-06-19 18:36:20 +0100

    rename hilt module

--- app/src/main/java/com/shevaalex/android/plugev/di/DataBinderModule.kt
+++ app/src/main/java/com/shevaalex/android/plugev/di/DataBinderModule.kt
@@ -10,7 +10,7 @@ import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
 @InstallIn(ViewModelComponent::class)
-abstract class MapScreenModule {
+abstract class DataBinderModule {
 
     @Binds
     @ViewModelScoped
