e2f56131744317a63c4d7c43364c45c446108645
AlexMason <shevaalex@yahoo.co.uk>
2021-06-19 17:42:47 +0100

    renames postcode field

--- app/src/main/java/com/shevaalex/android/plugev/data/network/postcodesio/model/PostCodeResult.kt
+++ app/src/main/java/com/shevaalex/android/plugev/data/network/postcodesio/model/PostCodeResult.kt
@@ -1,7 +1,9 @@
 package com.shevaalex.android.plugev.data.network.postcodesio.model
 
+import com.google.gson.annotations.SerializedName
+
 data class PostCodeResult(
-    val postcode: String,
+    @SerializedName("postcode") val postCodeName: String,
     val longitude: Double,
     val latitude: Double,
 )
