3139fe6dcf9d82fa574ac8044cf27270928dce8c
AlexMason <shevaalex@yahoo.co.uk>
2021-06-19 18:01:01 +0100

    adds mapping function to domain model

--- app/src/main/java/com/shevaalex/android/plugev/data/network/postcodesio/model/PostcodeNetworkDto.kt
+++ app/src/main/java/com/shevaalex/android/plugev/data/network/postcodesio/model/PostcodeNetworkDto.kt
@@ -1,9 +1,26 @@
 package com.shevaalex.android.plugev.data.network.postcodesio.model
 
+import com.google.android.gms.maps.model.LatLng
 import com.google.gson.annotations.SerializedName
+import com.shevaalex.android.plugev.domain.postcode.model.PostCode
 
 data class PostcodeNetworkDto(
     val status: Int,
     @SerializedName("result") val postCode: PostCodeResult?,
     val error: String?,
 )
+
+fun PostcodeNetworkDto.toDomainModel(): PostCode {
+    return if (this.postCode != null) {
+        PostCode.PostCodeSuccess(
+            status = this.status,
+            postCodeName = postCode.postCodeName,
+            position = LatLng(this.postCode.latitude, this.postCode.longitude)
+        )
+    } else {
+        PostCode.PostCodeError(
+            status = this.status,
+            error = this.error ?: "Invalid postcode"
+        )
+    }
+}
