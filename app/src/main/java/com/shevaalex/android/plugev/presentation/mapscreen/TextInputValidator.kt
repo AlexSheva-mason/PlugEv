107181ac64acbf4cf9f590b0b71a6b3c8cc76817
AlexMason <shevaalex@yahoo.co.uk>
2021-06-21 21:52:36 +0100

    adds char limit and uppercase conversion

--- app/src/main/java/com/shevaalex/android/plugev/presentation/mapscreen/TextInputValidator.kt
+++ app/src/main/java/com/shevaalex/android/plugev/presentation/mapscreen/TextInputValidator.kt
@@ -1,8 +1,13 @@
 package com.shevaalex.android.plugev.presentation.mapscreen
 
+import java.util.*
+
 fun returnValidatedTextForInput(input: String): String {
     if (input.isBlank()) return input
-    return input.filter {
-        it.isLetterOrDigit() || it.isWhitespace()
-    }
+    return input
+        .filter {
+            it.isLetterOrDigit() || it.isWhitespace()
+        }
+        .toUpperCase(Locale.getDefault())
+        .take(8)
 }
--- app/src/test/java/com/shevaalex/android/plugev/presentation/mapscreen/TextInputValidatorKtTest.kt
+++ app/src/test/java/com/shevaalex/android/plugev/presentation/mapscreen/TextInputValidatorKtTest.kt
@@ -7,15 +7,39 @@ import org.junit.Test
 class TextInputValidatorKtTest {
 
     @Test
-    fun `should not return characters that are not within a-z 0-9 or whitespace`(){
+    fun `should not return characters that are not within a-z 0-9 or whitespace`() {
         //GIVEN
-        val input = "!£$%^_ &*()Abc "
+        val input = "!£$%^_&*()A "
 
         //WHEN
         val result = returnValidatedTextForInput(input)
 
         //THEN
-        assertThat(result).isEqualTo(" Abc ")
+        assertThat(result).isEqualTo("A ")
+    }
+
+    @Test
+    fun `should return all letters in uppercase`() {
+        //GIVEN
+        val input = "aBcdEfg"
+
+        //WHEN
+        val result = returnValidatedTextForInput(input)
+
+        //THEN
+        assertThat(result).isEqualTo("ABCDEFG")
+    }
+
+    @Test
+    fun `should limit output to maximum 8 chars`() {
+        //GIVEN
+        val input = "ABCD EFGH"
+
+        //WHEN
+        val result = returnValidatedTextForInput(input)
+
+        //THEN
+        assertThat(result).isEqualTo("ABCD EFG")
     }
 
 }
