23d556ca22c91ea3e4078704914ad421f8891703
AlexMason <shevaalex@yahoo.co.uk>
2021-06-29 20:33:52 +0100

    fixes okhttp interceptor to log only in debug

--- app/src/main/java/com/shevaalex/android/plugev/data/common/di/DataModule.kt
+++ app/src/main/java/com/shevaalex/android/plugev/data/common/di/DataModule.kt
@@ -1,5 +1,6 @@
 package com.shevaalex.android.plugev.data.common.di
 
+import com.shevaalex.android.plugev.BuildConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,12 +16,13 @@ object DataModule {
     @Singleton
     @Provides
     fun provideOkhttpClient(): OkHttpClient {
-        return OkHttpClient.Builder()
-            .addInterceptor(
-                HttpLoggingInterceptor()
-                    .setLevel(HttpLoggingInterceptor.Level.BASIC)
+        val builder = OkHttpClient.Builder()
+        if (BuildConfig.DEBUG) {
+            builder.addInterceptor(
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
             )
-            .build()
+        }
+        return builder.build()
     }
 
 }
